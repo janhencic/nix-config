65e1ca1188c0f442bacb07645e3a11de2f9be828
janhencic <jan@hencic.com>
2023-02-26 12:42:19 +0100

    Add git to nix-shell

--- shell.nix
+++ shell.nix
@@ -14,6 +14,7 @@ default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
     nativeBuildInputs = with pkgs; [
       home-manager
-    ];
+      git 
+   ];
   };
 }
