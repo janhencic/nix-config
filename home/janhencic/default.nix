4f01a83a54421661546dc959204e3382f4e72cd8
janhencic <jan@hencic.com>
2023-02-26 18:59:41 +0100

    Add alacritty home-manager conifg

--- home/janhencic/default.nix
+++ home/janhencic/default.nix
@@ -48,4 +48,59 @@ in
     userName = "janhencic";
     userEmail = "jan@hencic.com";
   };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = {
+          x = 0;
+          y = 0;
+        };
+        dynamic_padding = true;
+      };
+      font = {
+        size = 12;
+        normal = {
+          family = "FiraCode Nerd Font Mono";
+          style = "Regular";
+        };
+        bold = {
+          family = "FiraCode Nerd Font Mono";
+        };
+        italic = {
+          family = "FiraCode Nerd Font Mono";
+        };
+      };
+      colors = {
+        primary = {
+          background = "#272b2e";
+          foreground = "#FCFCFA";
+        };
+        normal = {
+          black = "#403E41";
+          red = "#FF6188";
+          green = "#A9DC76";
+          yellow = "#FFD866";
+          blue = "#ffba69";
+          magenta = "#AB9DF2";
+          cyan = "#78DCE8";
+          white = "#FCFCFA";
+        };
+        bright = {
+          black = "#727072";
+          red = "#FF6188";
+          green = "#A9DC76";
+          yellow = "#FFD866";
+          blue = "#ffba69";
+          magenta = "#AB9DF2";
+          cyan = "#78DCE8";
+          white = "#FCFCFA";
+        };
+      };
+      cursor = {
+        underline = {};
+      };
+    };
+  };
 }
