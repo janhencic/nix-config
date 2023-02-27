21c1177f6976931a28a45d5f4157eeadb44933ef
janhencic <jan@hencic.com>
2023-02-27 23:55:28 +0100

    Remove duplicate user config

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -52,16 +52,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.janhencic = {
-    isNormalUser = true;
-    initialPassword = "123";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      firefox
-    ];
-  };
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
