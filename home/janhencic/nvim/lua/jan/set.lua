ad60838d9840fb80090dff7b218f33fd87c6ae72
janhencic <jan@hencic.com>
2023-03-04 15:04:39 +0100

    Set fixed width to signcolumn

--- home/janhencic/nvim/lua/jan/set.lua
+++ home/janhencic/nvim/lua/jan/set.lua
@@ -5,3 +5,6 @@ vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 -- Start with all folds open
 vim.o.foldlevelstart = 99
+-- If this is not set the column will pop in and out whenever lsp is displaying a warning
+-- see https://github.com/neovim/nvim-lspconfig/issues/195 for more information.
+vim.o.signcolumn = 'yes'
