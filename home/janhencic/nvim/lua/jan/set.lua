cfa828a08606e7b1ee462b1c036a03dfa2fbe9d4
janhencic <jan@hencic.com>
2023-03-24 22:55:42 +0100

    Add vnoremap to stay in visual mode when indenting

--- home/janhencic/nvim/lua/jan/set.lua
+++ home/janhencic/nvim/lua/jan/set.lua
@@ -8,4 +8,9 @@ vim.o.foldlevelstart = 99
 -- If this is not set the column will pop in and out whenever lsp is displaying a warning
 -- see https://github.com/neovim/nvim-lspconfig/issues/195 for more information.
 vim.o.signcolumn = 'yes'
+
 vim.o.relativenumber = true
+
+-- Stay in visual mode after indenting either left or right
+vim.cmd('vnoremap < <gv')
+vim.cmd('vnoremap > >gv')
