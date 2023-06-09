c7fc507109101c62d943fd12db0fe21c10f3ff56
janhencic <jan@hencic.com>
2023-06-09 06:42:22 +0200

    Introduce common host config

---
+++ hosts/common.nix
@@ -0,0 +1,84 @@
+{ lib, config, pkgs, inputs, ... }:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "nodev";
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Ljubljana";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.pathsToLink = [ "/libexec" ];
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    picom
+    pinentry-curses
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.mosh.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  nix.settings = {
+    substituters = [ "https://tvbeat-nixpkgs-cache.s3-eu-west-1.amazonaws.com/" ];
+    trusted-public-keys = [ "hydra.tvbeat.com:4iHmKDd95QN9Po2FzqmfUD11Wk0/ln1oLlaLXDaIsNE=" ];
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
+
+  hardware.opengl.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+  };
+
+  environment.sessionVariables = rec {
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    TERM="xterm-256color";
+  };
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  hardware.bluetooth.enable = true;
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    libinput.enable = true;
+    displayManager.lightdm.enable = true;
+    displayManager.session = [
+      {
+        manage = "desktop";
+        name = "xsession";
+        start = ''exec $HOME/.xsession'';
+      }
+    ];
+  };
+}
--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -6,47 +6,13 @@
       inputs.home-manager.nixosModules.home-manager
       ./hardware-configuration.nix
       ./users/janhencic
-    ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.device = "nodev";
+      ../common.nix
+    ];
 
   networking.hostName = "pc";
-  networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Ljubljana";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = lib.mkForce {
-  #  font = "Lat2-Terminus16";
-  #  keyMap = "us";
-  #  useXkbConfig = true; # use xkbOptions in tty.
-  #};
-
-  environment.pathsToLink = [ "/libexec" ];
-
-  # Enable the X11 windowing system.
   services.xserver = {
-    enable = true;
-    desktopManager = {
-      xterm.enable = false;
-    };
-   
-    libinput.enable = true;
-    displayManager.lightdm.enable = true;
-    displayManager.session = [
-      {
-        manage = "desktop";
-        name = "xsession";
-        start = ''exec $HOME/.xsession'';
-      }
-    ];
-
     # Set my preferred monitor resolution and refresh rate.
     # Taken from https://discourse.nixos.org/t/proper-way-to-configure-monitors
     displayManager.setupCommands = ''
@@ -58,40 +24,6 @@
     '';
   };  
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  environment.systemPackages = with pkgs; [
-    picom
-    pinentry-curses
-  ];
-
-  nixpkgs.config.allowUnfree = true;
-
-  programs.mosh.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -99,35 +31,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
-
-  nix.settings = {
-    substituters = [ "https://tvbeat-nixpkgs-cache.s3-eu-west-1.amazonaws.com/" ];
-    trusted-public-keys = [ "hydra.tvbeat.com:4iHmKDd95QN9Po2FzqmfUD11Wk0/ln1oLlaLXDaIsNE=" ];
-    experimental-features = [ "nix-command" "flakes" ];
-  };
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.modesetting.enable = true;
-
-  hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-  hardware.nvidia.prime = {
-    sync.enable = true;
-
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
-
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
-  };
-
-  environment.sessionVariables = rec {
-    XDG_CONFIG_HOME = "\${HOME}/.config";
-    TERM="xterm-256color";
-  };
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  hardware.bluetooth.enable = true;
 }
