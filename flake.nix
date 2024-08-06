aa5eecc1ef914317496548c2826c8ac00ddacc3f
janhencic <jan@hencic.com>
2024-08-06 17:49:26 +0200

    Update flake.lock and flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nil.url = "github:oxalica/nil";
@@ -63,5 +63,9 @@
             pkgs.nixpkgs-fmt
           ];
         };
+
+      # So I can inspect tham via nix repl
+      inherit nixpkgs;
+      inherit nixpkgs-unstable;
     };
 }
