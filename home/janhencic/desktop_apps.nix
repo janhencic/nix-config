ee884ceb0f43ace3afec974935c60dd29c56bce7
janhencic <jan@hencic.com>
2024-08-06 17:55:28 +0200

    Remove drata config

--- home/janhencic/desktop_apps.nix
+++ home/janhencic/desktop_apps.nix
@@ -1,16 +1,10 @@
 { pkgs, pkgs-unstable }:
 
-let
-  drata = (import ./drata {
-    pkgs = pkgs-unstable;
-    stdenv = pkgs-unstable.stdenv;
-  });
-in with pkgs; [
+with pkgs; [
   chromium
   deluge
   pavucontrol
   pkgs-unstable.slack
   pkgs-unstable.google-chrome
-  drata
   pkgs-unstable.postman
 ]
