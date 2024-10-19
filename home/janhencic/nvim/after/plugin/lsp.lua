8d4cc4d3aacacbc8d185f17d32cb0f48a8de0813
janhencic <jan@hencic.com>
2024-10-19 13:50:19 +0200

    fix: Replace outdated ts lsp server config name

--- home/janhencic/nvim/after/plugin/lsp.lua
+++ home/janhencic/nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local jan_lsp = require('jan.lsp')
 
 require('lspconfig')['lua_ls'].setup(jan_lsp.setup_lua)
 require('lspconfig')['rust_analyzer'].setup(jan_lsp.setup_ts)
-require('lspconfig')['tsserver'].setup(jan_lsp.setup_ts)
+require('lspconfig')['ts_ls'].setup(jan_lsp.setup_ts)
 require('lspconfig')['bashls'].setup(jan_lsp.bashls)
 require('lspconfig')['pyright'].setup(jan_lsp.pyright)
 require('lspconfig')['nil_ls'].setup(jan_lsp.nil_ls)
